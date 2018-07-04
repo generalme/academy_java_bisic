e9ace55bd2e2815ec4083d9e9c58d3b758e9f6cb
강병우 <PC382224@PC38224>
2018-07-04 17:51:41 +0900

    java day01

---
+++ day01-hello/SRC/operator/binary/PlusAssignment.java
@@ -0,0 +1,28 @@
+package operator.binary;
+
+/**
+ * 누적 대입 연산자중 += 연산자를 테스트
+ * @author PC382224
+ *
+ */
+public class PlusAssignment {
+
+	public static void main(String[] args) {
+		// 선언
+		int num = 0;
+		
+		// 사용
+		System.out.println("초기 num 값:" + num);
+		
+		num += 1;
+		// num = num +1;
+		System.out.println("num += 1 수행 후 :" + num); //1
+		num = num +1;
+	    System.out.println("num = num + 1 수행 후 :" + num); //
+	    num += num +1;
+	    System.out.println("num += num + 1 수행 후 :" + num);
+	    num *= num * num;
+	    System.out.println("num *= num * num 수행 후 :" + num);
+	}
+
+}
